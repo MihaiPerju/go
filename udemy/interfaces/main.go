df861976f9f3b5039e7019ecdaaeaf9e70a7715c
Michael Perju <mikeperju@gmail.com>
2021-06-07 04:52:21 +0300

    update

---
+++ udemy/interfaces/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type englishBot struct {
+}
+
+type spanishBot struct {
+}
+
+type bot interface {
+	getGreeting() string
+}
+
+func (eb englishBot) getGreeting() string {
+	return "Hello"
+}
+
+func (sb spanishBot) getGreeting() string {
+	return "Hola"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func main() {
+
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
